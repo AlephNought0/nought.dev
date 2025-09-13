7c001b70348ad535439e3ce8b4d7e1f592ea1c74
AlephNought0 <alephnought0@proton.me>
2025-09-13 05:17:38 +0200

    Learn how to actually use astro framework with bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config'
+// @ts-check
+import { defineConfig } from 'astro/config';
+
 import react from '@astrojs/react';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://nought.dev',
-
   integrations: [react()],
-})
+});
