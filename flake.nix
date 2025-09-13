f6be16dc74ce5dd5c9631e4e75128ae20101ba45
AlephNought0 <alephnought0@proton.me>
2025-09-13 03:50:52 +0200

    Add bun with react

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs.buildPackages; [
+        bun
+      ];
+    };
+  };
+}
